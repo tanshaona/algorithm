69203e6fc6f7aa19dba4c3ae935989e1ae8839c6
zhuanxu <wangchao19900130@126.com>
2013-12-25 20:27:56 +0800

    add some method,now it is nearly complete.

--- bstree.hs
+++ bstree.hs
@@ -3,14 +3,21 @@ data Tree a = Empty
 			deriving (Eq, Show)
 
 -- key (Node v l r) = v
-singlon v = Node v Empty Empty
+leaf v = Node v Empty Empty
+
+isEmpty Empty = True
+isEmpty _	  = False
 
 insert :: (Ord a) => Tree a -> a -> Tree a
-insert Empty k      = singlon k
+insert Empty k      = leaf k
 insert (Node v l r) k 	| k < v     = Node v (insert l k) r
 						| otherwise = Node v l (insert r k) 	 
 
--- fromList [x]    = singlon x
+mapT :: (t -> a) -> Tree t -> Tree a
+mapT f Empty        = Empty
+mapT f (Node v l r) = Node (f v) (mapT f l) (mapT f r)
+
+-- fromList [x]    = leaf x
 -- fromList (x:xs) = insert (fromList xs) x
 fromList :: Ord b => [b] -> Tree b
 fromList  = foldl insert Empty 
@@ -20,6 +27,26 @@ toList t 	 = flatten inorder t
 
 sortList 	:: Ord a => [a] -> [a]
 sortList 	 = toList . fromList
+
+lookupT :: Ord a => Tree a -> a -> Tree a
+lookupT Empty _ = Empty
+lookupT t@(Node v l r) k | k == v = t
+						| k <  v = lookupT l k
+						| k >  v = lookupT r k
+--maximumT :: Tree a -> Int
+maximumT = rightorder max (minBound :: Int) 
+minimumT = leftorder  min (maxBound :: Int)
+
+delete Empty _ = Empty
+delete (Node v l r) k 	| k < v = Node v (delete l k) r
+					 	| k > v = Node v l (delete r k)
+						-- x == k
+						| isEmpty l = r
+						| isEmpty r = l
+						| otherwise = Node k' (delete l k') r
+							where k' = maximumT l
+					--	| otherwise = Node k' l (delete r k')
+					--		where k' = minimumT r
 
 --toList (Node v l r) = (toList l) ++ [v] ++ (toList r)
 --
@@ -54,12 +81,33 @@ traverse step f z tree = go tree z
 
 preorder  = traverse (\n l r -> r . l . n)
 inorder   = traverse (\n l r -> r . n . l)
-postorder = traverse (\n l r -> r . n . l)
+postorder = traverse (\n l r -> n . r . l)
 
 flatten traversal = reverse . traversal (:) []
 
 printTreeAscend = flatten inorder
 
-leftorder = traverse (\n l r -> l . n)
+leftorder  = traverse (\n l r -> l . n)
+rightorder = traverse (\n l r -> r . n)  
 -- leftorder min maxBound tree
 
+
+-- now I want to give the preorder given the inorder and postorder
+rebuild [] _		= Empty
+-- rebuild [p] _ 		= leaf p 
+rebuild (p:ps) is 	= Node p (rebuild lp li) (rebuild rp ri)
+				where 
+					li = takeWhile (p /=) is
+					ni = length li 
+					ri = drop (ni+1) is
+					(lp,rp) = splitAt ni ps
+--postOrder :: [a] -> [a] -> [a]
+--postOrder  = toList . rebuild
+
+testRebuild = "\ntest rebuild tree, output the post-order result giving the in-order result and pre-order result" ++ 
+			  "\n pre-order:\t" ++ show pre_order ++
+ 			  "\n in-order:\t" ++ show in_order ++
+			  "\n post-order:\t" ++ show (flatten postorder $ rebuild pre_order in_order)
+		where 
+				pre_order = [1,2,4,3,5,6]
+			 	in_order  = [4,2,1,5,3,6]
