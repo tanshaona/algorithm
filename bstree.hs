cb822ebd63bada033092c1e69e04504ef9f28d61
zhuanxu <wangchao19900130@126.com>
2013-12-25 15:32:15 +0800

    innner dependece in the tree search

--- bstree.hs
+++ bstree.hs
@@ -21,7 +21,22 @@ inorderTraversal f z tree = go tree z
 									z''   = f  v z'
 									z'''  = go r z''
 								in z'''
+inorderTraversal2 f z tree = go tree z
+	where 
+		go Empty		z = z
+		go (Node v l r) z = go r . f v . go l $ z
+
+traverse step f z tree = go tree z
+		where
+			go Empty		z = z
+			go (Node v l r) z = step (f v) (go l) (go r) z
+
+preorder  = traverse (\n l r -> r . l . n)
+inorder   = traverse (\n l r -> r . n . l)
+postorder = traverse (\n l r -> r . n . l)
 
 flatten traversal = reverse . traversal (:) []
 
+leftorder = traverse (\n l r -> l . n)
+-- leftorder min maxBound tree
 
