e93f1bdd12bc9b9c6e8f42a2b96d68e382c01acc
zhuanxu <tanshaona@126.com>
2014-01-03 10:09:32 +0800

    add newTon method.

--- lec2a.hs
+++ lec2a.hs
@@ -7,11 +7,13 @@ mapReduce fn accu zero a b
 		| otherwise = accu (fn a) (mapReduce fn accu zero (a+1) b)
 -}
 
+mapReduce fn accu zero a b = foldl (\a x -> accu a (fn x)) zero [a..b]
+{-
 mapReduce fn accu zero a b = loop a zero
 	where loop itor acc 
 				| itor > b  = acc
 				| otherwise = loop (itor+1) (accu acc (fn itor))
-
+-}
 findFixPoint f x = 	if isGoodEnough x next 
 					then next
 					else findFixPoint f next
@@ -22,6 +24,16 @@ findFixPoint f x = 	if isGoodEnough x next
 -- what it really matters here is that
 -- the procedure averageDump take a procedure and then produces a procedure
 sqrtMe x = findFixPoint (averageDump (\y -> x/y)) 1.0
-			where averageDump f = (\z -> ((f z) + z)/2 )
+		--	where averageDump f = (\z -> ((f z) + z)/2 )
+        --  why I alter it from the above to the below is that
+        --  it can clearly describle the really procedure that takes a procedure and returns a  procedure
+            where averageDump = (\f -> (\x -> average (f x) x))
+                  average x y = (x + y)/2
+sqrtMe2 x = newTonMethod (\y -> x - y*y) 1.0
 
-
+newTonMethod f guess = findFixPoint (\y -> y - (f y)/(df y)) guess
+--    where df x = (f (x+dx) - f x) / dx 
+--          dx   = 0.00001
+    where df = deriv f
+          deriv = (\f -> (\x -> (f (x+dx) - (f x)) / dx))
+          dx = 0.00001
